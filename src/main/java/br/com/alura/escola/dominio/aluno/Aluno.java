40d90fe2f87884c6ea1edd2bdc7245c1bf538293
Lucas <lucasmarcos343@gmail.com>
2022-01-04 10:29:27 -0400

    Criando interface RepositoriodeAlunos

--- src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
@@ -1,4 +1,4 @@
-package br.com.alura.escola.aluno;
+package br.com.alura.escola.dominio.aluno;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,5 +19,23 @@ public class Aluno {
 	public void adicionarTelefone(String ddd, String numero) {
 		this.telefones.add(new Telefone(ddd, numero));
 	}
+
+	public CPF getCpf() {
+		return cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Email getEmail() {
+		return email;
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+	
+	
 
 }
